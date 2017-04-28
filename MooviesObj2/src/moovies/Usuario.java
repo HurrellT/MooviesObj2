a4254e7ef3c4711783c2c8d7b2d1b996dc0cacf2
RafaelMamani <rafael040795@gmail.com>
2017-04-28 18:35:11 -0300

    x

--- MooviesObj2/src/moovies/Usuario.java
+++ MooviesObj2/src/moovies/Usuario.java
@@ -1,7 +1,42 @@
 package moovies;
 
-public class Usuario {
-	
-	//Prueba
+import java.util.ArrayList;
+import java.util.Map;
 
+public class Usuario {
+	// nombre y apellido
+	private String nyap;
+	// edad
+	private int edad;
+	// ocupacion
+	private String ocupacion;
+	// codigo postal
+	private int codPos;
+	// genero favorito
+	private String generoFavorito;
+	//amigos del usuarios
+	private ArrayList<Interger> amigos;
+	//peliculas ??
+	private Map<Interger,Interger> pelisVistas;
+	//id
+	private int id;
+	
+	
+	public ArrayList<Interger> getAmigos() {
+		return amigos;
+	}
+	
+	public Map<Interger, Interger> getPelis() {
+		return Pelis;
+	}
+	
+	public void calificarPelicula(Interger,int){
+		
+	}
+	
+	public void agregarAmigo(Interger){}
+	
 }
+	public void eliminarAmigo(Interger){
+		
+	}
