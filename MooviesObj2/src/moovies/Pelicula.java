6c81926e0ff8ca6bb7a0bb485c241455e07072c5
tomas <tomas@tlh-note>
2017-04-28 18:05:12 -0300

    Implementacion de Pelicula

--- MooviesObj2/src/moovies/Pelicula.java
+++ MooviesObj2/src/moovies/Pelicula.java
@@ -1,5 +1,68 @@
 package moovies;
 
-public class Pelicula {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
 
+/*
+ * Clase que define una Pelicula.
+ * 		La misma tiene nombre, año de estreno, un id, sus generos,
+ * 		y los puntajes que usuarios le dan. 
+ * @author TLH
+ */
+
+public class Pelicula {
+	
+	// nombre de la pelicula
+	private String nombre;
+	
+	// año de estreno
+	private Integer añoDeEstreno;
+	
+	// ID
+	private Integer id;
+	
+	// generos
+	private ArrayList<String> generos;
+	
+	// puntajes
+	private Map<Integer,Integer> puntajes;
+	
+	
+	// Constructor de Pelicula
+	public Pelicula(String name,
+			Integer year,
+			Integer idNum, 
+			ArrayList<String> genres,
+			Map<Integer, Integer> score) {
+		
+		name 	= nombre;
+		year 	= añoDeEstreno;
+		idNum 	= id;
+		genres	= generos;
+		score	= puntajes;
+	}
+	
+	//Getters
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public Integer getAñoDeEstreno() {
+		return añoDeEstreno;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public ArrayList<String> getGeneros() {
+		return generos;
+	}
+	
+	public Map<Integer, Integer> getPuntajes() {
+		return puntajes;
+	}
+	
 }
