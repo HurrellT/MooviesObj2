21cdb459cff77c81d5407fd345caa90f330dcba5
tomas <tomas@tlh-note>
2017-04-28 16:20:24 -0300

    Testeando push

---
+++ MooviesObj2/src/moovies/MooviesTest.java
@@ -0,0 +1,17 @@
+package moovies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MooviesTest {
+
+	@Test
+	public void test() {
+		
+		String test = Test.prueba();
+		
+		assertArrayEquals("Test", test);
+	}
+
+}
---
+++ MooviesObj2/src/moovies/Test.java
@@ -0,0 +1,15 @@
+package moovies;
+
+public class Test {
+	
+	public void prueba() {
+		mensajePrueba();
+	}
+
+	private String mensajePrueba() {
+		
+		String mensaje = "Test";
+		
+		return (mensaje);
+	}
+}
