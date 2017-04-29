561973c8e6fd02ef0e6fbfc4c278bbf635201a4f
Brian <leonel89011@gmail.com>
2017-04-29 20:39:55 -0300

    Update Moovies.java

--- MooviesObj2/src/moovies/Moovies.java
+++ MooviesObj2/src/moovies/Moovies.java
@@ -1,5 +1,11 @@
 package moovies;
 
 public class Moovies {
+  
+    //lista de usuarios
+    private ArrayList<Usuario> usuarios;
+  
+    //lista de peliculas
+    private ArrayList<Pelicula> peliculas;
 
 }
