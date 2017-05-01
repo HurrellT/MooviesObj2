2971c307c9c10ffc00a2e3468cb20346446dcc11
Brian <leonel89011@gmail.com>
2017-04-30 21:11:18 -0300

    Actualizacion clase CSVFileReader

--- MooviesObj2/src/CSVFileReaders/CSVFileReader.java
+++ MooviesObj2/src/CSVFileReaders/CSVFileReader.java
@@ -3,23 +3,25 @@ package CSVFileReaders;
 import java.util.*;
 import java.io.*;
 import java.util.stream.*;
+
 /**
- * It reads a CSV File and returns an object from each line read. It is up to the subclasses to
- * define how that object gets created.
- */
+* It reads a CSV File and returns an object from each line read. 
+* It is up to the subclasses to define how that object gets created.
+*/
+
 public abstract class CSVFileReader<T> {
     private String filePath;
 
     /**
-     * @param filePath the absolute path of the file to be read.
-     * */
+    * @param filePath the absolute path of the file to be read.
+    **/
     public CSVFileReader(String filePath){
         this.filePath = filePath;
     }
 
     /**
-     * Reads a CSV file and returns a list of T
-     * */
+    * Reads a CSV file and returns a list of T
+    **/
     public List<T> readFile(){
         Stream<String> lines = this.readLines();
 
@@ -29,40 +31,42 @@ public abstract class CSVFileReader<T> {
 
 
     /**
-     * Reads the lines from the file and returns them as a Stream.
-     * If the file doesn't exist it returns an empty Stream.
-     * */
+    * Reads the lines from the file and returns them as a Stream.
+    * If the file doesn't exist it returns an empty Stream.
+    **/
     private Stream<String> readLines(){
-        FileReader fileReader           = null;
+        FileReader fileReader = null;
         try {
             fileReader = new FileReader(this.filePath);
-        } catch (FileNotFoundException e) {
+        } 
+        catch (FileNotFoundException e) {
             System.out.println(this.filePath + " NO EXISTE. \nFlashaste.");
             e.printStackTrace();
             return new ArrayList<String>().stream();
         }
         BufferedReader bufferedReader   = new BufferedReader(fileReader);
-
+        
         return bufferedReader.lines();
+        
     }
 
     /**
      * A valid regex string (remember escaping especial characters in regex)
-     * */
+    **/
     protected String getSeparator(){
         return "\\|";
     }
 
     /**
      * Applies the separator to a given line
-     * */
+    **/
     private String[] splitLine(String line){
         return line.split(this.getSeparator());
     }
 
     /**
      * Returns an Object of type T. Must be redefined by subclasses.
-     * */
-    protected  abstract T parseLine(String[] line);
+    **/
+    protected abstract T parseLine(String[] line);
 }
 
