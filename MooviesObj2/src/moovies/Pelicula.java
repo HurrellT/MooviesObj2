72d8d74bfea17a1ab16e459bd4797a90f358e4f3
Brian <leonel89011@gmail.com>
2017-04-30 21:08:41 -0300

    Actualizacion clase Pelicula

--- MooviesObj2/src/moovies/Pelicula.java
+++ MooviesObj2/src/moovies/Pelicula.java
@@ -48,10 +48,10 @@ public class Pelicula {
  	*/
 	
 	public Pelicula(int id,
-					String name,
-					int year,
-					int idmbNum, 
-					ArrayList<String> genres) {
+			String name,
+			int year,
+			int idmbNum, 
+			ArrayList<String> genres) {
 		//Map<Integer, Integer> score
 		//la pelicula no deberia recibir los puntajes al ser creada
 		//estos los agregan los usuarios a medida que califican la pelicula
@@ -100,8 +100,10 @@ public class Pelicula {
 	*/
 	
 	public void addRating(int puntaje, int id){
-		
-	
+		Integer rating = new Integer(puntaje);
+		Integer idUsuario = new Integer(id); 
+		//hay que hacer esto porque puntajes requiere dos Integer
+		this.puntajes.put(idUsuario, rating);	
 	}
 	
 }
