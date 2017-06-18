c20b5688a97f28203d22f70ef082ce197a5588a0
mega_ <mega_@192.168.0.25>
2017-06-18 16:48:33 -0300

    Init UsuarioTest

--- MooviesObj2/test/mooviesTests/UsuarioTest.java
+++ MooviesObj2/test/mooviesTests/UsuarioTest.java
@@ -1,10 +1,22 @@
 package mooviesTests;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import moovies.Usuario;
 
 public class UsuarioTest {
 
+	Usuario user;
+	Usuario user2;
+	
+	@Before
+	private void setUp() {
+		
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
